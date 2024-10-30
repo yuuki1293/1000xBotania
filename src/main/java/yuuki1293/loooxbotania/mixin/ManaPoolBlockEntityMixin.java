d8cc8d902b944fba9487b7ddd5a4b8ba945c8efc
yuuki1293 <yuuki1293@outlook.jp>
2024-10-30 20:12:25 +0900

    mixin

---
+++ src/main/java/yuuki1293/loooxbotania/mixin/ManaPoolBlockEntityMixin.java
@@ -0,0 +1,9 @@
+package yuuki1293.loooxbotania.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import vazkii.botania.common.block.block_entity.mana.ManaPoolBlockEntity;
+
+@Mixin(ManaPoolBlockEntity.class)
+public class ManaPoolBlockEntityMixin {
+}

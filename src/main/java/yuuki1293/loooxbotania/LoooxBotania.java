46ccb3c4db6fe7539f17ae9cb8ea67e2ce55369a
yuuki1293 <yuuki1293@outlook.jp>
2024-10-31 10:49:25 +0900

    remove unused

--- src/main/java/yuuki1293/loooxbotania/LoooxBotania.java
+++ src/main/java/yuuki1293/loooxbotania/LoooxBotania.java
@@ -1,18 +1,9 @@
 package yuuki1293.loooxbotania;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -28,9 +19,5 @@ public class LoooxBotania
     public LoooxBotania()
     {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
     }
 }
